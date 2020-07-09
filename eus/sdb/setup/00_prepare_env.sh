785388ab3bd1bc7c963208e55d07558391668b9b
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-07-09 13:10:11 +0200

    update sdb scripts

---
+++ eus/sdb/setup/00_prepare_env.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: 00_prepare_env.sh
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2020.07.07
+# Revision...: 
+# Purpose....: Script to prepare environment e.g. domain name etc
+# Notes......: ...
+# Reference..: --
+# License....: Licensed under the Universal Permissive License v 1.0 as 
+#              shown at https://oss.oracle.com/licenses/upl.
+# ---------------------------------------------------------------------------
+# Modified...:
+# see git revision history for more information on changes/updates
+# ---------------------------------------------------------------------------
+# - Customization -----------------------------------------------------------
+export DOMAIN=${DOMAIN:-"trivadislabs.com"} 
+export OUD_HOST=${OUD_HOST:-"eusoud.${DOMAIN}"}
+export BASEDN=${BASEDN:-"dc=$(echo $DOMAIN|cut -d. -f1),dc=$(echo $DOMAIN|cut -d. -f2)"} 
+# - End of Customization ----------------------------------------------------
+
+# - Environment Variables ---------------------------------------------------
+ORACLE_BASE=${ORACLE_BASE:-"/u00/app/oracle"}
+TNS_ADMIN=${TNS_ADMIN:-"${ORACLE_BASE}/network/admin"}
+DEFAULT_HOST=$(hostname 2>/dev/null ||cat /etc/hostname ||echo $HOSTNAME)
+
+# get the fqdn hostname if necessary
+if [[ ${DEFAULT_HOST//[^.]} != "" ]]; then 
+    HOST=$DEFAULT_HOST
+else 
+    HOST=$DEFAULT_HOST.$DOMAIN
+fi
+
+# - EOF Environment Variables -----------------------------------------------
+
+# - Main --------------------------------------------------------------------
+echo "= Prepare SQLNet Environment =========================================="
+
+if [ -f "$TNS_ADMIN/sqlnet.ora" ]; then
+    echo "- update sqlnet.ora ---------------------------------------------------"
+    sed -i "/^NAMES.DEFAULT_DOMAIN=/{h;s/=.*/=$DOMAIN/};\${x;/^$/{s//NAMES.DEFAULT_DOMAIN=$DOMAIN/;H};x}" $TNS_ADMIN/sqlnet.ora
+fi
+
+echo "- update listener.ora -----------------------------------------------"
+if [ -f "$TNS_ADMIN/listener.ora" ]; then
+    cp -v $TNS_ADMIN/listener.ora $TNS_ADMIN/listener.ora.orig
+    sed -i "s/(HOST =.*)/(HOST = $HOST )/" $TNS_ADMIN/listener.ora
+    $ORACLE_HOME/bin/lsnrctl reload
+fi
+
+echo "- update ldap.ora ---------------------------------------------------"
+if [ -f "$TNS_ADMIN/ldap.ora" ]; then
+    cp -v $TNS_ADMIN/ldap.ora $TNS_ADMIN/ldap.ora.orig
+fi
+
+cat << EOF >$TNS_ADMIN/ldap.ora
+DIRECTORY_SERVERS=(${OUD_HOST}:1389:1636)
+DEFAULT_ADMIN_CONTEXT = "${BASEDN}"
+DIRECTORY_SERVER_TYPE = OID
+EOF
+
+echo "- Configure db_domain in Database ${ORACLE_SID} ---------------------"
+${ORACLE_HOME}/bin/sqlplus -S -L /nolog <<EOFSQL
+connect / as sysdba
+SELECT value FROM v\$parameter WHERE name='db_domain';
+ALTER SYSTEM SET db_domain="${DOMAIN}" SCOPE=SPFILE;
+SHUTDOWN IMMEDIATE;
+STARTUP;
+SELECT value FROM v\$parameter WHERE name='db_domain';
+exit;
+EOFSQL
+echo "= Finish update SQLNet Environment ===================================="
+# --- EOF --------------------------------------------------------------------
--- eus/sdb/setup/08_eus_registration.sh
+++ eus/sdb/setup/08_eus_registration.sh
@@ -19,34 +19,97 @@
 # -----------------------------------------------------------------------
 
 export BASEDN=${BASEDN:-"dc=trivadislabs,dc=com"}
-#export OUD_HOST=${OUD_HOST:-"oud.trivadislabs.com"}
-export OUD_HOST=${OUD_HOST:-"eusoud.trivadislabs.com"}
+export DOMAIN=${DOMAIN:-"trivadislabs.com"} 
+export OUD_HOST=${OUD_HOST:-"eusoud.${DOMAIN}"}
+export OUD_PORT=${OUD_PORT:-1389}
+export OUD_PORT_SSL=${OUD_PORT_SSL:-1636}
 export ORACLE_SID=${ORACLE_SID:-"TEUS01"}
-export EUS_ADMIN=${EUS_ADMIN:-"$(cat /u01/common/etc/oud_eus_eusadmin_dn.txt)"}
-export EUS_PWD_FILE=${EUS_PWD_FILE:-"/u01/common/etc/oud_eus_eusadmin_pwd.txt"}
+export TNS_ADMIN=${TNS_ADMIN:-"${ORACLE_BASE}/network/admin"}
+export EUS_ADMIN=${EUS_ADMIN:-"$(cat /u01/common/etc/eusadmin_dn.txt)"}
+export EUS_PWD_FILE=${EUS_PWD_FILE:-"/u01/common/etc/eusadmin_pwd.txt"}
 export SYS_PWD_FILE=${SYS_PWD_FILE:-"${ORACLE_BASE}/admin/${ORACLE_SID}/etc/${ORACLE_SID}_password.txt"}
-export WALLET_PWD_FILE=${WALLET_PWD_FILE:-"${ORACLE_BASE}/admin/${ORACLE_SID}/etc/${ORACLE_SID}_password.txt"}
+export WALLET_PWD_FILE=${WALLET_PWD_FILE:-"${ORACLE_BASE}/admin/${ORACLE_SID}/etc/${ORACLE_SID}_wallet_pwd.txt"}
 
 # - configure SQLNet ----------------------------------------------------
 echo "Configure SQLNet ldap.ora:"
 echo "  BASEDN              :   ${BASEDN}"
 echo "  OUD_HOST            :   ${OUD_HOST}"
 
-echo "DIRECTORY_SERVERS=(${OUD_HOST}:1389:1636)"    > ${ORACLE_BASE}/network/admin/ldap.ora
-echo "DEFAULT_ADMIN_CONTEXT = \"${BASEDN}\""        >>${ORACLE_BASE}/network/admin/ldap.ora    
-echo "DIRECTORY_SERVER_TYPE = OID"                  >>${ORACLE_BASE}/network/admin/ldap.ora
+if [ -f "$TNS_ADMIN/ldap.ora" ]; then
+    cp -v $TNS_ADMIN/ldap.ora $TNS_ADMIN/ldap.ora.orig
+fi
 
-# - configure instance --------------------------------------------------
-echo "Register Database ${ORACLE_SID} in OUD using:"
-echo "  ORACLE_SID          :   ${ORACLE_SID}"
-echo "  EUS_ADMIN           :   ${EUS_ADMIN}"
-echo "  SYS_PWD_FILE        :   ${SYS_PWD_FILE}"
-echo "  WALLET_PWD_FILE     :   ${WALLET_PWD_FILE}"
-echo "  EUS_PWD_FILE        :   ${EUS_PWD_FILE}"
-$ORACLE_HOME/bin/dbca -silent -configureDatabase \
--sourceDB ${ORACLE_SID} -sysDBAUserName sys -sysDBAPassword $(cat ${SYS_PWD_FILE}) \
--registerWithDirService true -dirServiceUserName ${EUS_ADMIN} \
--dirServicePassword $(cat ${EUS_PWD_FILE}) -walletPassword $(cat ${WALLET_PWD_FILE}) 
+cat << EOF >$TNS_ADMIN/ldap.ora
+DIRECTORY_SERVERS=(${OUD_HOST}:${OUD_PORT}:${OUD_PORT_SSL})
+DEFAULT_ADMIN_CONTEXT = "${BASEDN}"
+DIRECTORY_SERVER_TYPE = OID
+EOF
+
+# reuse existing password file
+if [ -f "$WALLET_PWD_FILE" ]; then
+    echo "- found wallet password file ${WALLET_PWD_FILE}"
+    export ADMIN_PASSWORD=$(cat ${WALLET_PWD_FILE})
+# use default password from variable
+elif [ -n "${DEFAULT_WALLET_PASSWORD}" ]; then
+    echo "- use default wallet password from \${DEFAULT_WALLET_PASSWORD}"
+    echo ${DEFAULT_WALLET_PASSWORD}> ${WALLET_PWD_FILE}
+    export ADMIN_PASSWORD=$(cat $WALLET_PWD_FILE)
+    # still here, then lets create a password
+else 
+    # Auto generate a password
+    echo "- auto generate new wallet password..."
+    while true; do
+        s=$(cat /dev/urandom | tr -dc "A-Za-z0-9" | fold -w 10 | head -n 1)
+        if [[ ${#s} -ge 8 && "$s" == *[A-Z]* && "$s" == *[a-z]* && "$s" == *[0-9]*  ]]; then
+            echo "- passwort does Match the criteria"
+            break
+        else
+            echo "- password does not Match the criteria, re-generating..."
+        fi
+    done
+    echo "- use auto generated wallet password"
+    ADMIN_PASSWORD=$s
+    echo "- save wallet password in ${WALLET_PWD_FILE}"
+    echo ${ADMIN_PASSWORD}>$WALLET_PWD_FILE
+fi
+
+# check if database is registered
+echo "- check if ${ORACLE_SID} does exist in OracleContext ${BASEDN}"
+DATABASE_DN=$(ldapsearch -h ${OUD_HOST} -p ${OUD_PORT} -D ${EUS_ADMIN} -w $(cat ${EUS_PWD_FILE}) -b ${BASEDN} -s sub "(cn=${ORACLE_SID})" dn 2>/dev/null)
+if [ -n "${DATABASE_DN}" ]; then
+# wenn vorhanden check dn in wallet
+    echo "- reset password for database ${ORACLE_SID} in OracleContext ${BASEDN}"
+    $ORACLE_HOME/bin/dbca -silent -configureDatabase \
+        -sourceDB ${ORACLE_SID} -sysDBAUserName sys -sysDBAPassword $(cat ${SYS_PWD_FILE}) \
+        -regenerateDBPassword true -dirServiceUserName "${EUS_ADMIN}" \
+        -dirServicePassword $(cat ${EUS_PWD_FILE}) -walletPassword $(cat ${WALLET_PWD_FILE}) 
+else
+    # check if we do have a old wallet
+    WALLET_DIR="${ORACLE_BASE}/admin/${ORACLE_SID}/wallet"
+    TIMESTAMP="$(date '+%y%m%d_%H%M')"
+    if [ -f "${WALLET_DIR}/ewallet.p12" ]; then
+        mv -v ${WALLET_DIR}/ewallet.p12 ${WALLET_DIR}/ewallet.p12_${TIMESTAMP}
+    fi
+    if [ -f "${WALLET_DIR}/cwallet.sso" ]; then
+        mv -v ${WALLET_DIR}/cwallet.sso ${WALLET_DIR}/cwallet.sso_${TIMESTAMP}
+    fi
+    echo "- register database ${ORACLE_SID} in OracleContext ${BASEDN}"
+    $ORACLE_HOME/bin/dbca -silent -configureDatabase \
+        -sourceDB ${ORACLE_SID} -sysDBAUserName sys -sysDBAPassword $(cat ${SYS_PWD_FILE}) \
+        -registerWithDirService true -dirServiceUserName "${EUS_ADMIN}" \
+        -dirServicePassword $(cat ${EUS_PWD_FILE}) -walletPassword $(cat ${WALLET_PWD_FILE}) 
+fi
+
+UNIQUEMEMBER=$(ldapsearch -h ${OUD_HOST} -p ${OUD_PORT} -D ${EUS_ADMIN} -w $(cat ${EUS_PWD_FILE}) -b ${BASEDN} -s sub "(&(cn=OracleDefaultDomain)(uniquemember=cn=${ORACLE_SID},cn=OracleContext,dc=trivadislabs,dc=com))" uniquemember 2>/dev/null)
+if [ -z "${UNIQUEMEMBER}" ]; then
+    echo "- add database ${ORACLE_SID} to the OracleDefaultDomain"
+    ldapmodify -h ${OUD_HOST} -p ${OUD_PORT} -D ${EUS_ADMIN} -w $(cat ${EUS_PWD_FILE}) <<LDIF
+dn: cn=OracleDefaultDomain,cn=OracleDBSecurity,cn=Products,cn=OracleContext,${BASEDN}
+changetype: modify
+add: uniqueMember
+uniqueMember: cn=${ORACLE_SID},cn=OracleContext,${BASEDN}
+LDIF
+fi
 
 # - unlock system -------------------------------------------------------
 $ORACLE_HOME/bin/sqlplus -S -L /NOLOG <<EOFSQL
